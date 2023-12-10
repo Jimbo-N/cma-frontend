02352aace688623a549f810fcba5726fce19fafa
25240 <2524086063@qq.com>
2023-12-10 20:57:02 +0800

    个人信息

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,15 +13,26 @@
         <el-col :span="2">
           <el-button :type="$route.path === '/equipmentlib' ? 'primary' : 'text'" @click="gotoEquipmentlib">设备库</el-button>
         </el-col>
-        <el-col :span="3" class="user-display">
-          <el-avatar icon="el-icon-user-solid"></el-avatar>
+        <el-col :span="3" class="user-display"  >
+            <el-avatar icon="el-icon-user-solid" @click.native="dialogVisible = true;"></el-avatar>
             <div class="user-info">
               <p class="user-id">{{ userid }}</p>
               <p class="user-name">{{ realname }}</p>
             </div>
         </el-col>
       </el-row>
+      <el-dialog title="个人信息" :visible.sync="dialogVisible" :modal-append-to-body="false" >
+        <div class="user-info">
+              <p class="user-id">工号：{{ userid }}</p>
+              <p class="user-name">姓名：{{ realname }}</p>
+         </div>
+         <span slot="footer" class="dialog-footer">
+              <el-button type="primary" @click="gotoEdit(); dialogVisible = false">修改密码</el-button>
+              <el-button @click="dialogVisible = false">确定</el-button>
+            </span>
+        </el-dialog>
     </el-header>
+    
   </template>
   
   <script>
@@ -30,12 +41,14 @@
       return {
         realname: localStorage.getItem('realname'),
         userid: localStorage.getItem('userid'),
+        dialogVisible: false
       };
     },
     methods: {
       gotoMain() {
         if (this.$route.path !== "/projects") {
           this.$router.push("/projects");
+          
         }
       },
       gotoEmp() {
@@ -47,7 +60,11 @@
       gotoEquipmentlib() {
         this.$router.push("/equipmentlib");
       },
+      gotoEdit(){
+        this.$router.push("/edit");
+      }
     },
+
   };
   </script>
   
@@ -66,6 +83,9 @@
    /* box-shadow: 0 2px 4px rgba(0,0,0,.1);  */
     }
     .header img{
+      cursor: pointer;
+    }
+    .el-avatar{
       cursor: pointer;
     }
     .header .el-button{
